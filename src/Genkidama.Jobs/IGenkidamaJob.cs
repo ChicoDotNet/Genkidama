b3ede5bf6d78ec7d2d68c1908b0a477cd6a1fc98
Alfonso Lara Ramos <ChicoDotNet@users.noreply.github.com>
2026-07-02 21:15:46 -0600

    GEN-008: add job contract

---
+++ src/Genkidama.Jobs/IGenkidamaJob.cs
@@ -0,0 +1,9 @@
+namespace Genkidama.Jobs;
+
+/// <summary>
+/// Represents a background job request.
+/// </summary>
+/// <typeparam name="TResponse">The response type.</typeparam>
+public interface IGenkidamaJob<TResponse>
+{
+}
