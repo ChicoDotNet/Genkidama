f696063a8272fa339717e4144914cbe2fe49191b
Alfonso Lara Ramos <ChicoDotNet@users.noreply.github.com>
2026-07-02 21:08:05 -0600

    GEN-007: add command contract

---
+++ src/Genkidama.Application/IGenkidamaCommand.cs
@@ -0,0 +1,9 @@
+namespace Genkidama.Application;
+
+/// <summary>
+/// Represents a command that changes application state.
+/// </summary>
+/// <typeparam name="TResponse">The response type.</typeparam>
+public interface IGenkidamaCommand<TResponse>
+{
+}
