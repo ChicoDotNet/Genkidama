9ca97103b5f79d3e6baed04827c81a8da9bdfeb3
Alfonso Lara Ramos <ChicoDotNet@users.noreply.github.com>
2026-08-15 02:36:18 -0600

    fix(kotlin): migrate Android module to built-in Kotlin

--- learn/es/kotlin/android/app/build.gradle.kts
+++ learn/es/kotlin/android/app/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     id("com.android.application")
-    id("org.jetbrains.kotlin.android")
     id("org.jetbrains.kotlin.plugin.compose")
     id("com.google.devtools.ksp")
 }
@@ -25,10 +24,6 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-}
-
-kotlin {
-    jvmToolchain(17)
 }
 
 dependencies {
