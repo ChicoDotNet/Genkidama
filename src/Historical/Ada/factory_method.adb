e461e3802129bdc4c9150998f72a8545dcd44f32
Alfonso Lara Ramos <ChicoDotNet@users.noreply.github.com>
2026-08-18 15:08:01 -0600

    fix(patterns): preserve Ada Factory Method product polymorphism

--- src/Historical/Ada/factory_method.adb
+++ src/Historical/Ada/factory_method.adb
@@ -1,33 +1,48 @@
 with Ada.Text_IO; use Ada.Text_IO;
 
 procedure Factory_Method is
-   type Database_Kind is (Postgres, MySQL);
-   type Factory_Method_Access is access function return Database_Kind;
+   type Database_Action is access procedure;
 
-   function Create_Postgres return Database_Kind is (Postgres);
-   function Create_MySQL return Database_Kind is (MySQL);
+   type Database_Product is record
+      Connect : Database_Action;
+      Query   : Database_Action;
+   end record;
 
-   procedure Connect (Database : Database_Kind) is
+   type Factory_Method_Access is access function return Database_Product;
+
+   procedure Postgres_Connect is
    begin
-      case Database is
-         when Postgres => Put_Line ("PostgreSQL connect");
-         when MySQL    => Put_Line ("MySQL connect");
-      end case;
-   end Connect;
+      Put_Line ("PostgreSQL connect");
+   end Postgres_Connect;
 
-   procedure Query (Database : Database_Kind) is
+   procedure Postgres_Query is
    begin
-      case Database is
-         when Postgres => Put_Line ("PostgreSQL query");
-         when MySQL    => Put_Line ("MySQL query");
-      end case;
-   end Query;
+      Put_Line ("PostgreSQL query");
+   end Postgres_Query;
+
+   procedure MySQL_Connect is
+   begin
+      Put_Line ("MySQL connect");
+   end MySQL_Connect;
+
+   procedure MySQL_Query is
+   begin
+      Put_Line ("MySQL query");
+   end MySQL_Query;
+
+   function Create_Postgres return Database_Product is
+     (Connect => Postgres_Connect'Access,
+      Query   => Postgres_Query'Access);
+
+   function Create_MySQL return Database_Product is
+     (Connect => MySQL_Connect'Access,
+      Query   => MySQL_Query'Access);
 
    procedure Use_Database (Create_Database : Factory_Method_Access) is
-      Database : constant Database_Kind := Create_Database.all;
+      Database : constant Database_Product := Create_Database.all;
    begin
-      Connect (Database);
-      Query (Database);
+      Database.Connect.all;
+      Database.Query.all;
    end Use_Database;
 
 begin
