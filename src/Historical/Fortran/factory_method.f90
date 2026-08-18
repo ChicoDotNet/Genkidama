bae5a4106eca39d239545ebeb45a4e4c60f4e093
Alfonso Lara Ramos <ChicoDotNet@users.noreply.github.com>
2026-08-18 15:08:09 -0600

    fix(patterns): preserve Fortran Factory Method product polymorphism

--- src/Historical/Fortran/factory_method.f90
+++ src/Historical/Fortran/factory_method.f90
@@ -2,8 +2,20 @@ program factory_method
   implicit none
 
   abstract interface
-    integer function factory_method_proc()
-    end function factory_method_proc
+    subroutine database_action()
+    end subroutine database_action
+  end interface
+
+  type :: database_product
+    procedure(database_action), pointer, nopass :: connect => null()
+    procedure(database_action), pointer, nopass :: query => null()
+  end type database_product
+
+  abstract interface
+    subroutine factory_method_proc(database)
+      import :: database_product
+      type(database_product), intent(out) :: database
+    end subroutine factory_method_proc
   end interface
 
   call use_database(create_postgres)
@@ -11,29 +23,41 @@ program factory_method
 
 contains
 
-  integer function create_postgres()
-    create_postgres = 1
-  end function create_postgres
+  subroutine postgres_connect()
+    print '(A)', 'PostgreSQL connect'
+  end subroutine postgres_connect
 
-  integer function create_mysql()
-    create_mysql = 2
-  end function create_mysql
+  subroutine postgres_query()
+    print '(A)', 'PostgreSQL query'
+  end subroutine postgres_query
+
+  subroutine mysql_connect()
+    print '(A)', 'MySQL connect'
+  end subroutine mysql_connect
+
+  subroutine mysql_query()
+    print '(A)', 'MySQL query'
+  end subroutine mysql_query
+
+  subroutine create_postgres(database)
+    type(database_product), intent(out) :: database
+    database%connect => postgres_connect
+    database%query => postgres_query
+  end subroutine create_postgres
+
+  subroutine create_mysql(database)
+    type(database_product), intent(out) :: database
+    database%connect => mysql_connect
+    database%query => mysql_query
+  end subroutine create_mysql
 
   subroutine use_database(create_database)
     procedure(factory_method_proc) :: create_database
-    integer :: database
+    type(database_product) :: database
 
-    database = create_database()
-    select case (database)
-    case (1)
-      print '(A)', 'PostgreSQL connect'
-      print '(A)', 'PostgreSQL query'
-    case (2)
-      print '(A)', 'MySQL connect'
-      print '(A)', 'MySQL query'
-    case default
-      error stop 'unknown database'
-    end select
+    call create_database(database)
+    call database%connect()
+    call database%query()
   end subroutine use_database
 
 end program factory_method
