6a5a57ee1c1ed148e6f27617ed734205dc7b8010
Alfonso Lara Ramos <ChicoDotNet@users.noreply.github.com>
2026-08-21 13:13:22 -0600

    style(patterns): rustfmt Bridge example

--- src/Systems/Rust/bridge.rs
+++ src/Systems/Rust/bridge.rs
@@ -5,24 +5,40 @@ trait Device {
 
 struct TvDevice;
 impl Device for TvDevice {
-    fn power_on(&self) -> &'static str { "TV:on" }
-    fn mute(&self) -> &'static str { "TV:muted" }
+    fn power_on(&self) -> &'static str {
+        "TV:on"
+    }
+    fn mute(&self) -> &'static str {
+        "TV:muted"
+    }
 }
 
 struct RadioDevice;
 impl Device for RadioDevice {
-    fn power_on(&self) -> &'static str { "Radio:on" }
-    fn mute(&self) -> &'static str { "Radio:muted" }
+    fn power_on(&self) -> &'static str {
+        "Radio:on"
+    }
+    fn mute(&self) -> &'static str {
+        "Radio:muted"
+    }
 }
 
-struct BasicRemote<'a, D: Device> { device: &'a D }
+struct BasicRemote<'a, D: Device> {
+    device: &'a D,
+}
 impl<D: Device> BasicRemote<'_, D> {
-    fn activate(&self) -> &'static str { self.device.power_on() }
+    fn activate(&self) -> &'static str {
+        self.device.power_on()
+    }
 }
 
-struct MuteRemote<'a, D: Device> { device: &'a D }
+struct MuteRemote<'a, D: Device> {
+    device: &'a D,
+}
 impl<D: Device> MuteRemote<'_, D> {
-    fn activate(&self) -> &'static str { self.device.mute() }
+    fn activate(&self) -> &'static str {
+        self.device.mute()
+    }
 }
 
 fn main() {
