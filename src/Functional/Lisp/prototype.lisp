b27a08f9bbf2d78ed81457e51336a8517199c56f
Alfonso Lara Ramos <ChicoDotNet@users.noreply.github.com>
2026-08-20 22:03:02 -0600

    fix(patterns): avoid Common Lisp DESCRIBE collision

--- src/Functional/Lisp/prototype.lisp
+++ src/Functional/Lisp/prototype.lisp
@@ -7,7 +7,7 @@
    :name (service-profile-name profile)
    :features (copy-list (service-profile-features profile))))
 
-(defun describe (profile)
+(defun describe-profile (profile)
   (format nil "~a: ~{~a~^,~}"
           (service-profile-name profile)
           (service-profile-features profile)))
@@ -17,5 +17,5 @@
   (setf (service-profile-name canary) "orders-canary")
   (setf (service-profile-features canary)
         (append (service-profile-features canary) (list "tracing")))
-  (format t "original=~a~%" (describe original))
-  (format t "clone=~a~%" (describe canary)))
+  (format t "original=~a~%" (describe-profile original))
+  (format t "clone=~a~%" (describe-profile canary)))
