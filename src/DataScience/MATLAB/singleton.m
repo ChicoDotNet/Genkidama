47bcf91d7e3ec037577d28d689fe5b03002dce01
Alfonso Lara Ramos <ChicoDotNet@users.noreply.github.com>
2026-08-21 04:06:55 -0600

    fix(patterns): separate MATLAB Singleton handle class

--- src/DataScience/MATLAB/singleton.m
+++ src/DataScience/MATLAB/singleton.m
@@ -14,19 +14,3 @@ function value = registry_instance()
     end
     value = shared;
 end
-
-classdef RegistryState < handle
-    properties (Access = private)
-        CountValue = 0
-    end
-
-    methods
-        function increment(self)
-            self.CountValue = self.CountValue + 1;
-        end
-
-        function value = count(self)
-            value = self.CountValue;
-        end
-    end
-end
