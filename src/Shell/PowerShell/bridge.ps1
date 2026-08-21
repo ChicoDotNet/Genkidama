cfa88eaaeaca327b66d63ab15ece226b0fac7ad9
Alfonso Lara Ramos <ChicoDotNet@users.noreply.github.com>
2026-08-21 12:04:13 -0600

    feat(patterns): add PowerShell Bridge example

---
+++ src/Shell/PowerShell/bridge.ps1
@@ -0,0 +1,19 @@
+Set-StrictMode -Version Latest
+$ErrorActionPreference = 'Stop'
+
+function New-Device([string]$Name) {
+    @{
+        TurnOn = { "$Name:on" }.GetNewClosure()
+        Mute   = { "$Name:muted" }.GetNewClosure()
+    }
+}
+
+function Invoke-BasicRemote($Device) { & $Device.TurnOn }
+function Invoke-MuteRemote($Device) { & $Device.Mute }
+
+$tv = New-Device 'TV'
+$radio = New-Device 'Radio'
+"basic-tv=$(Invoke-BasicRemote $tv)"
+"basic-radio=$(Invoke-BasicRemote $radio)"
+"mute-tv=$(Invoke-MuteRemote $tv)"
+"mute-radio=$(Invoke-MuteRemote $radio)"
