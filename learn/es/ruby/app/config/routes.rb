262de98a97e57457604f914aa1c501424d889ee4
Alfonso Lara Ramos <ChicoDotNet@users.noreply.github.com>
2026-08-22 11:34:55 -0600

    feat(learn-ruby): route contact transfer operations

--- learn/es/ruby/app/config/routes.rb
+++ learn/es/ruby/app/config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   root "contacts#index"
 
   resources :contacts, only: %i[index show new create edit update] do
+    collection do
+      get :export
+      post :import
+    end
     resources :notes, only: :create
   end
 end
