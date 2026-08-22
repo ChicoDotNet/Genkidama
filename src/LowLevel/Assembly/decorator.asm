6c5a0e49a135911de66872be2e7a2c84a64aff39
Alfonso Lara Ramos <ChicoDotNet@users.noreply.github.com>
2026-08-22 11:00:06 -0600

    Fix NASM Decorator data label

--- src/LowLevel/Assembly/decorator.asm
+++ src/LowLevel/Assembly/decorator.asm
@@ -1,15 +1,15 @@
 global _start
 
 section .data
-    out db "base=alert",10,"audit=audit(alert)",10,"encrypted=enc(alert)",10,"stacked=audit(enc(alert))",10
-    out_len equ $-out
+    output_text db "base=alert",10,"audit=audit(alert)",10,"encrypted=enc(alert)",10,"stacked=audit(enc(alert))",10
+    output_len equ $-output_text
 
 section .text
 _start:
     mov rax, 1
     mov rdi, 1
-    mov rsi, out
-    mov rdx, out_len
+    mov rsi, output_text
+    mov rdx, output_len
     syscall
 
     mov rax, 60
