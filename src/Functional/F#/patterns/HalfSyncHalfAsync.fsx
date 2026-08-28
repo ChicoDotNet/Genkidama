72ae68b54057d44dfa96a543d6eda0fa2deaebfd
Alfonso Lara Ramos <ChicoDotNet@users.noreply.github.com>
2026-08-28 04:20:51 -0600

    Fix F# Half-Sync canonical tokenization

--- src/Functional/F#/patterns/HalfSyncHalfAsync.fsx
+++ src/Functional/F#/patterns/HalfSyncHalfAsync.fsx
@@ -1,2 +1,6 @@
 module HalfSyncHalfAsyncExample
-let run ()=["job-1";"job-2";"job-3"]|>List.map(fun j->$"done:{j}")|>String.concat ">"|>(=)"done:job-1>done:job-2>done:job-3"
+let run () =
+    [ "job-1"; "job-2"; "job-3" ]
+    |> List.map (fun job -> $"done:{job}")
+    |> String.concat ">"
+    |> (=) "done:job-1>done:job-2>done:job-3"
