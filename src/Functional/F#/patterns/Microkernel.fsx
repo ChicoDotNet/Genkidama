00df755d5b7dc0a0c259b545f11f88d393a1288e
Alfonso Lara Ramos <ChicoDotNet@users.noreply.github.com>
2026-08-28 04:19:21 -0600

    Clarify F# Microkernel indexing and invocation

--- src/Functional/F#/patterns/Microkernel.fsx
+++ src/Functional/F#/patterns/Microkernel.fsx
@@ -1,2 +1,4 @@
 module MicrokernelExample
-let run ()=let plugins=dict["double",(fun x->x*2);"square",(fun x->x*x)] in plugins["double"]4=8&&plugins["square"]4=16
+let run () =
+    let plugins = dict [ "double", (fun x -> x * 2); "square", (fun x -> x * x) ]
+    plugins["double"] 4 = 8 && plugins["square"] 4 = 16
