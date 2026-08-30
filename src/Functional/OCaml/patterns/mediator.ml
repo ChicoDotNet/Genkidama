24d12f4a6347c3aa006e97d305f0b3d5c4e3ca23
Alfonso Lara Ramos <ChicoDotNet@users.noreply.github.com>
2026-08-30 07:02:53 -0600

    patterns: strengthen OCaml mediator example

--- src/Functional/OCaml/patterns/mediator.ml
+++ src/Functional/OCaml/patterns/mediator.ml
@@ -1,7 +1,34 @@
+exception Unknown_colleague of string
+
+type receiver = sender:string -> message:string -> string
+
+type mediator = (string, receiver) Hashtbl.t
+
+let register mediator name receiver = Hashtbl.replace mediator name receiver
+
+let send mediator ~sender ~recipient ~message =
+  match Hashtbl.find_opt mediator recipient with
+  | Some receiver -> receiver ~sender ~message
+  | None -> raise (Unknown_colleague recipient)
+
 let () =
-  let events = ref [] in
-  let mediate sender message =
-    events := (sender ^ ":" ^ message) :: !events
+  let mediator = Hashtbl.create 2 in
+  register mediator "payment" (fun ~sender ~message ->
+      Printf.sprintf "payment received %s from %s" message sender);
+  register mediator "inventory" (fun ~sender ~message ->
+      Printf.sprintf "inventory received %s from %s" message sender);
+
+  assert (
+    send mediator ~sender:"payment" ~recipient:"inventory" ~message:"paid"
+    = "inventory received paid from payment");
+  assert (
+    send mediator ~sender:"inventory" ~recipient:"payment" ~message:"reserved"
+    = "payment received reserved from inventory");
+
+  let rejected_unknown =
+    try
+      ignore (send mediator ~sender:"payment" ~recipient:"shipping" ~message:"paid");
+      false
+    with Unknown_colleague "shipping" -> true
   in
-  mediate "checkout" "paid";
-  assert (!events = [ "checkout:paid" ])
+  assert rejected_unknown
