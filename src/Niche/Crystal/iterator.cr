cc1b50d1043ed34d035e0df14de8d429684de112
Alfonso Lara Ramos <ChicoDotNet@users.noreply.github.com>
2026-08-29 19:01:59 -0600

    patterns(iterator): make Crystal example reusable

--- src/Niche/Crystal/iterator.cr
+++ src/Niche/Crystal/iterator.cr
@@ -5,17 +5,25 @@ class CursorIterator
 
   def next_value : Int32?
     return nil if @index >= @values.size
+
     value = @values[@index]
     @index += 1
     value
   end
 end
 
-iterator = CursorIterator.new([10, 20, 30])
-visited = [] of Int32
-while value = iterator.next_value
-  visited << value
+def run_iterator_example : Bool
+  iterator = CursorIterator.new([10, 20, 30])
+  visited = [] of Int32
+  while value = iterator.next_value
+    visited << value
+  end
+
+  visited == [10, 20, 30] && iterator.next_value.nil?
 end
 
-raise "iterator contract failed" unless visited == [10, 20, 30] && iterator.next_value.nil?
-puts "iterator=#{visited.join(',')}"
+if PROGRAM_NAME == __FILE__
+  raise "iterator contract failed" unless run_iterator_example
+
+  puts "iterator=10,20,30"
+end
